85ab3e1af5661f34beb47c51d33a40a4d5662928
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-03 10:41:22 +0100

    day 3

--- src/03/main.py
+++ src/03/main.py
@@ -32,11 +32,57 @@ total = 0
 result = []
 table = new_table(None, width=2, height=4)
 data=[]
+test = """vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw"""
+
+
+# for i, line in enumerate(test.split("\n")):
+#     data.append(tuple([x for x in chunks(list(line), len(line)//2)]))
 with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
     for i, line in enumerate(f):
         line = line.strip()
         if line:
-            # data.append(int(line))
-            data.append({"line": i, "value": int(line)})
+            data.append(tuple([x for x in chunks(list(line), len(line)//2)]))
 
-    print(data)
+
+print(data)
+
+def intersection(lst1, lst2):
+    lst3 = [value for value in lst1 if value in lst2]
+    return list(set(lst3))
+
+items = []
+for half1, half2 in data:
+    inter = intersection(half1, half2)[0]
+    items.append(inter)
+
+values = list(ascii_lowercase+ascii_uppercase)
+value = 0
+for item in items:
+    value += values.index(item) +1
+
+print(value)
+
+
+groups = [g for g in chunks(data, 3)]
+
+items_2 = []
+for group in groups:
+    e1 = list([item for sublist in group[0] for item in sublist])
+    e2 = list([item for sublist in group[1] for item in sublist])
+    e3 = list([item for sublist in group[2] for item in sublist])
+
+    e12 = intersection(e1,e2)
+    e123 = intersection(e12,e3)
+    items_2.append(e123[0])
+
+
+value = 0
+for item in items_2:
+    value += values.index(item) +1
+
+print(value)

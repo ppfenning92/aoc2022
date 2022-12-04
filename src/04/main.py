1188aef265fd886e35c34886dcebc024699cd9ed
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-04 14:56:08 +0100

    day 4

--- src/04/main.py
+++ src/04/main.py
@@ -32,11 +32,39 @@ total = 0
 result = []
 table = new_table(None, width=2, height=4)
 data=[]
+test = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
 with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
     for i, line in enumerate(f):
+    # for i, line in enumerate(test.split("\n")):
         line = line.strip()
         if line:
-            # data.append(int(line))
-            data.append({"line": i, "value": int(line)})
+            ranges = line.split(',')
+            print(ranges)
+            r1 = parse_nums(ranges[0], False)
+            r2 = parse_nums(ranges[1], False)
+            ass1 = set(list(range(r1[0], r1[1]+1)))
+            ass2 = set(list(range(r2[0], r2[1]+1)))
+            data.append((ass1,ass2))
 
-    print(data)
+print(data)
+
+res = 0
+for a1, a2 in data:
+    if a1.issubset(a2) or a2.issubset(a1):
+        res += 1
+
+print('res1', res)
+
+
+overlaps = 0
+for a1, a2 in data:
+    if a1.intersection(a2) or a2.intersection(a1):
+        overlaps += 1
+
+print('overlaps', overlaps)

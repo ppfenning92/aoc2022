47aaefe66644d984c9e97c46a8b68ecad342f637
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-08 08:35:11 +0100

    day 8

--- src/08/main.py
+++ src/08/main.py
@@ -32,11 +32,113 @@ total = 0
 result = []
 table = new_table(None, width=2, height=4)
 data=[]
+test = """30373
+25512
+65332
+33549
+35390"""
+
+
 with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
     for i, line in enumerate(f):
+    # for i, line in enumerate(test.split('\n')):
         line = line.strip()
         if line:
-            # data.append(int(line))
-            data.append({"line": i, "value": int(line)})
+            data.append([int(t) for t in list(line)])
 
-    print(data)
+
+WIDTH = len(data[1])
+HEIGHT = len(data)
+vis = 2*HEIGHT + 2*(WIDTH-2)
+
+"""
+5 5 1
+5 3 3 
+3 5 4
+"""
+# def check_vis(h, trees:[int]):
+#     global  vis
+#     return all(t < h for t in trees)
+# def check_dir(x, y):
+#     h = data[x][y]
+#
+#     comp = []
+#     # top
+#     for _x in range(x):
+#         comp.append(data[_x][y])
+#     if check_vis(h, comp):
+#         return True
+#     comp = []
+#     # bot
+#     for _x in range(x+1,HEIGHT):
+#         comp.append(data[_x][y])
+#     if check_vis(h, comp):
+#         return True
+#     comp = []
+#     # right
+#     for _y in range(y+1, WIDTH):
+#         comp.append(data[x][_y])
+#     if check_vis(h, comp):
+#         return True
+#     comp = []
+#     # left
+#     for _y in range(y):
+#         comp.append(data[x][_y])
+#     if check_vis(h, comp):
+#         return True
+
+def check_dir(x, y):
+    h = data[x][y]
+    t = r = b = l = 0
+    comp = []
+    for _x in range(x):
+    # top
+        comp.append(data[_x][y])
+    comp.reverse()
+    for c in comp:
+        t += 1
+        if c >= h:
+            break
+
+    comp = []
+    for _x in range(x+1,HEIGHT):
+    # bot
+        comp.append(data[_x][y])
+    for c in comp:
+        b += 1
+        if c >= h:
+            break
+
+    comp = []
+    for _y in range(y+1, WIDTH):
+    # right
+        comp.append(data[x][_y])
+    for c in comp:
+        r += 1
+        if c >= h:
+            break
+
+    comp = []
+    for _y in range(y):
+    # left
+        comp.append(data[x][_y])
+    comp.reverse()
+    for c in comp:
+        l += 1
+        if c >= h:
+            break
+
+    print(h, '-', (t,l,b,r))
+    return t * r * b * l
+
+
+best = []
+for i in range(1,WIDTH-1):
+    for j in range(1,HEIGHT-1):
+        # if check_dir(i, j):
+        #     vis += 1
+        best.append(check_dir(i,j))
+
+# print(vis)
+print(best)
+print(max(best))

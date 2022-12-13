124d9716bd65a68ce8a6922b4162a33ee0ee3de5
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-13 14:38:43 +0100

    day 13

--- src/13/main.py
+++ src/13/main.py
@@ -27,13 +27,102 @@ import numpy as np
 
 total = 0
 table = new_table(None, width=2, height=4)
-data=[]
-test = """TEST"""
+test = """[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]"""
+data={1:()}
 with fileinput.input(files=(f"input.txt",), encoding="utf-8") as f:
-    for i, line in enumerate(test.split('\n')):
-    # for i, line in enumerate(f):
+    pair = 1
+    # for idx, line in enumerate(test.split('\n')):
+    for i, line in enumerate(f):
         line = line.strip()
         if line:
-            data.append(line)
+            data[pair] += (eval(line),)
+        else:
+            pair += 1
+            data[pair] = ()
 
 print(data)
+
+
+def in_order(l: [int] | int, r: [int] | int):
+    if type(l) == type(r) == list:
+        if len(l) > 0 and len(r) > 0:
+            cont = in_order(l[0], r[0])
+            if cont == 'continue':
+                return in_order(l[1:], r[1:])
+            else:
+                return cont
+        return 'continue' if (len(l) == len(r)) else (len(l) < len(r))
+    if type(l) == type(r) == int:
+        return 'continue' if (l == r) else (l < r)
+    else:
+        if type(l) == int:
+            return in_order([l], r)
+        if type(r) == int:
+            return in_order(l, [r])
+
+        raise Exception('not possible')
+
+pairs_in_order = []
+for _id, _pair in data.items():
+    left, right = _pair
+    print(_id,'# '*10)
+    if in_order(left, right):
+        pairs_in_order.append(_id)
+
+# print(pairs_in_order)
+print('part 1:', sum(pairs_in_order))
+
+
+DIV_1 = [[2]]
+DIV_2 = [[6]]
+data_2 = []
+for left, right in data.values():
+    data_2.append(left)
+    data_2.append(right)
+data_2 += [DIV_1, DIV_2]
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    swapped = False
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if not in_order(arr[j], arr[j + 1]):
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        if not swapped:
+            return
+
+
+bubble_sort(data_2)
+
+idx_1 = idx_2 = 0
+for idx, el in enumerate(data_2):
+    if el == DIV_1:
+        idx_1 = idx +1
+    if el == DIV_2:
+        idx_2 = idx +1
+
+print(idx_1, idx_2, idx_1*idx_2)

1b234ade6b23722562f9d200fda604e96142612f
Patrick Pfenning <patrick.pfenning.92@gmail.com>
2022-12-19 08:26:18 +0100

    askldoakjsdsa

--- src/17/main.py
+++ src/17/main.py
@@ -6,6 +6,9 @@ import os  # NOQA
 import sys  # NOQA
 
 import os, sys
+
+from tqdm import tqdm
+
 current_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
@@ -30,7 +33,7 @@ class Stone:
     falling: bool = True
     height: int
     width: int
-    pos: (int, int) = (0, 2) # top-left part of stone
+    pos: (int, int) = (-1, 2) # top-left part of stone
     def __init__(self, shape):
         self.shape = shape
         self.height = len(self.shape)
@@ -75,14 +78,15 @@ def can_stone_fall(_stone: Stone) -> bool:
     _y, _x = _stone.pos
     if _y+_stone.height > len(FIELD)-1:
         return False
-    for _c in range(len(_stone.shape[-1])):
-        stone_part = _stone.shape[-1][_c]
-        rr = _stone.height + _y
-        cc = _c + _x - 1
-        field_pos = FIELD[rr][cc]
-        if stone_part == 1:
-            if field_pos == '#':
-                return False
+    for _r in range(len(_stone.shape)):
+        for _c in range(len(_stone.shape[_r])):
+            stone_part = _stone.shape[_r][_c]
+            rr = _stone.height + _y
+            cc = _c + _x
+            field_pos = FIELD[rr][cc]
+            if stone_part == 1:
+                if field_pos == '#':
+                    return False
     return True
 
 def can_stone_move_left(_stone: Stone):
@@ -97,7 +101,7 @@ def can_stone_move_left(_stone: Stone):
     return True
 def can_stone_move_right(_stone: Stone):
     _y, _x = stone.pos
-    if (x + _stone.width + 1) >= 7:
+    if (x + _stone.width) >= 7:
         return False
     for _r in range(len(_stone.shape)):
         for _c in range(len(_stone.shape[_r])):
@@ -109,43 +113,78 @@ def can_stone_move_right(_stone: Stone):
                 if FIELD[_r + _y][_c + _x + 1] == '#':
                     return False
     return True
-for rock in range(4):
+
+def _draw_current(_stone: Stone, action: str):
+    for _r in range(y, y + stone.height):
+        for _c in range(x, x + stone.width):
+            FIELD[_r][_c] = '@' if stone.shape[_r - y][_c - x] == 1 else FIELD[_r][_c]
+
+    # print(action)
+    # print_grid(FIELD)
+    # print('')
+    _reset_draw(_stone)
+
+def _reset_draw(_stone: Stone):
+    for r_idx in range(len(FIELD)):
+        for c_idx in range(len(FIELD[r_idx])):
+            FIELD[r_idx][c_idx] = '.' if FIELD[r_idx][c_idx] != '#' else '#'
+
+
+def update_field(_stone: Stone):
+    global FIELD
+    emtpy_rows = 0
+    for row in FIELD:
+        if all(cell == '.' for cell in row):
+            emtpy_rows += 1
+        else:
+            break
+
+    FIELD = FIELD[emtpy_rows:]
+    for _ in range(3):
+        FIELD.insert(0, ['.', '.', '.', '.', '.', '.', '.'])
+
+for rock in tqdm(range(10)):
     stone = Stone(shapes[rock % len(shapes)])
 
     for _ in range(stone.height):
         FIELD.insert(0,['.', '.', '.', '.', '.', '.', '.'])
 
-    print(stone)
-
     while stone.falling:
         y,x = stone.pos
         jet = jets[step % len(jets)]
 
-        if jet == '>':
-            if can_stone_move_right(stone):
-                x += 1
-
-        if jet == '<':
-            if can_stone_move_left(stone):
-                x += -1
-
-        for r in range(y, y + stone.height):
-            for c in range(x, x + stone.width):
-                FIELD[r][c] = '@' if stone.shape[r-y][c-x] == 1 else FIELD[r][c]
-
-        print_grid(FIELD, str)
-        print(y)
-
         if can_stone_fall(stone):
-            stone.pos = (y+1,x)
+            y += 1
+            stone.pos = (y, x)
+            _draw_current(stone, 'fall')
         else:
             stone.falling = False
-        for r_idx in range(len(FIELD)):
-            for c_idx in range(len(FIELD[r_idx])):
-                FIELD[r_idx][c_idx] = '.' if FIELD[r_idx][c_idx] != '#' else '#'
-                if not stone.falling:
+
+        if stone.falling:
+            if jet == '>' and can_stone_move_right(stone):
+                    x += 1
+                    stone.pos = (y, x)
+                    _draw_current(stone, 'right')
+            elif jet == '<' and can_stone_move_left(stone):
+                    x += -1
+                    stone.pos = (y, x)
+                    _draw_current(stone, 'left')
+            else:
+                _draw_current(stone, 'noop')
+            step += 1
+
+        else:
+            for r_idx in range(len(FIELD)):
+                for c_idx in range(len(FIELD[r_idx])):
+                    FIELD[r_idx][c_idx] = '.' if FIELD[r_idx][c_idx] != '#' else '#'
                     for r in range(y, y + stone.height):
                         for c in range(x, x + stone.width):
                             FIELD[r][c] = '#' if stone.shape[r - y][c - x] == 1 else '.'
 
-        step += 1
+        stone.pos = (y,x)
+
+    update_field(stone)
+
+print_grid(FIELD)
+
+print(len(FIELD))
